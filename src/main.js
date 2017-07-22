506ea9f3bb76535bbc8ac442fe2eda988a5ecb1a
fefuns <1321120469@qq.com>
2017-07-22 23:27:19 +0800

    chore: 安装其他的一些项目依赖

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+
+import 'normalize.css/normalize.css'
+import fastclick from 'fastclick'
+
+fastclick.attach(document.body)
 
 Vue.config.productionTip = false
 
